8b8c72a72c027e3764688aefaf65cb9528eeec02
ciennce <maxileonhoppe@googlemail.com>
2026-09-14 10:02:45 +0200

    Refractored Lend();

--- OOP-Practice-2 - Bibliothek/Library/Library.cs
+++ OOP-Practice-2 - Bibliothek/Library/Library.cs
@@ -4,12 +4,6 @@ namespace OOP_Practice_2___Bibliothek.Library
 {
     class Library
     {
-
-        public Library()
-        {
-            
-        }
-
         private readonly List<Media> _mediaStorage = new();
         private readonly List<Member> _members = new();
 
@@ -18,69 +12,18 @@ namespace OOP_Practice_2___Bibliothek.Library
         {
             
             var member = _members.FirstOrDefault(m => m.MemberId == memberId);
-            if (member == null)
-            {
-                return LendResult.MemberNotFound;
-            }
-            else
-            {
-                if (member.Type.Count >= 3)
-                {
-                    return LendResult.LimitReached;
-                }
-                else
-                {
-                    var media = _mediaStorage.FirstOrDefault(i => i.Id == mediaId);
-                    if (!media.IsAvailable || media == null)
-                    {
-                        return LendResult.NotAvailable;
-                    }
-                    else
-                    {
-                        if (!media.IsLendable || media == null)
-                        {
-                            return LendResult.NotLendable;
-                        }
-                        else
-                        {
-                            member.Type.Add(media);
-                            return LendResult.Ok;
+            if (member == null) return LendResult.MemberNotFound;
 
-                        }
-                    }
-                }
-            }
+            var media = _mediaStorage.FirstOrDefault(i => i.Id == mediaId);
+            if (media == null) return LendResult.MediaNotFound;
 
+            if (!media.IsAvailable) return LendResult.NotAvailable;
+            if (member.Type.Count >= 3) return LendResult.LimitReached;
+            if(!media.IsLendable) return LendResult.NotLendable;
 
-
-
-
-
-
-            //else
-            //{
-            //    var media = _mediaStorage.FirstOrDefault(i => i.Id == mediaId);
-            //    if (media == null)
-            //    {
-            //        return LendResult.
-            //    }
-            //    else
-            //    {
-            //        if (media.IsAvailable)
-            //        {
-            //            media.LendTo(member);
-            //            return LendResult.
-            //        }
-            //        else
-            //        {
-            //            return 
-            //        }
-            //
-            //    }
-            //}    
-            
+            media.LendTo(member);
+            member.Type.Add(media);
+            return LendResult.Ok;
         }
-
-
-    }
+    }  
 }
