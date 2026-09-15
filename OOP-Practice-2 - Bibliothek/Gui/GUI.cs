26a4dfec2a4455a8d5037f5d94b4a074780c59b5
ciennce <maxileonhoppe@googlemail.com>
2026-09-15 16:06:52 +0200

    Added ReadInt()

--- OOP-Practice-2 - Bibliothek/Gui/GUI.cs
+++ OOP-Practice-2 - Bibliothek/Gui/GUI.cs
@@ -1,6 +1,17 @@
-﻿namespace OOP_Practice_2___Bibliothek.Gui
+﻿using OOP_Practice_2___Bibliothek.Library;
+using System.Data.SqlTypes;
+
+namespace OOP_Practice_2___Bibliothek.Gui
 {
     class GUI
     {
+        public LendResult ReadInt(string prompt)
+        {
+            bool hasWhiteSpace = prompt.Any(char.IsWhiteSpace);
+            bool isValid = !hasWhiteSpace && uint.TryParse(prompt, out _);
+
+            if (!isValid) return LendResult.Invalid;
+            return LendResult.Ok;
+        }    
     }
 }
